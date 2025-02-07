bc7b08db25a04f905853f5dd6637cf4808aae122
javapub <iswangshiyu@foxmail.com>
2025-02-07 21:41:06 +0800

    布局调整，调试用户信息获取

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,11 +1,33 @@
 // pages/home/home.js
 Page({
   data: {
-
+    avatar: '', // 头像图片路径
+    username: '',
+    memberStatus: '非学员',
+    // 轮播图
+    images: [
+      "/image/cover_1.jpg",
+      "/image/cover_2.jpg",
+      "/image/cover_3.jpg",
+    ]
   },
   onLoad: function (options) {
-
   },
+  getUserInfo: function() {
+    wx.getUserProfile({
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途
+      success: (res) => {
+        this.setData({
+          avatar: res.userInfo.avatarUrl,
+          username: res.userInfo.nickName
+        });
+      },
+      fail: (err) => {
+        console.log('获取用户信息失败', err);
+      }
+    });
+  },
+
   toTestPage: function(e){
     let testId = e.currentTarget.dataset['testid'];
     wx.navigateTo({
--- pages/index/index.js
+++ pages/index/index.js
@@ -5,9 +5,13 @@ const app = getApp()
 Page({
   data: {
     motto: 'Hello World',
-    userInfo: {},
+    userInfo: {
+      avatarUrl: '/image/wechat.png', // 默认头像
+      nickName: '微信用户' // 默认昵称
+    },
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUseGetUserProfile: true
+    // canIUseGetUserProfile: wx.canIUse('getUserProfile')
   },
   //事件处理函数
   bindViewTap: function() {
@@ -18,45 +22,55 @@ Page({
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
-        userInfo: app.globalData.userInfo,
+        userInfo: app.getUserInfo(),
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+    } else if (this.data.canIUseGetUserProfile) {
+      // 使用 getUserProfile 接口获取用户信息
+      // this.getUserProfile()
+    }
+  },
+  getUserProfile: function() {
+    wx.getUserProfile({
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途
+      success: (res) => {
+        app.globalData.userInfo = res.userInfo
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
         })
+      },
+      fail: (err) => {
+        console.log('获取用户信息失败', err);
+        wx.showModal({
+          title: '警告',
+          content: '1您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
+          showCancel: false,
+          confirmText: '返回授权',
+          success: function (res) {
+            // 用户没有授权成功，不需要改变 isHide 的值
+            if (res.confirm) {
+              console.log('用户点击了“返回授权”');
+            }
+          }
+        });
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    });
   },
   getUserInfo: function(e) {
     console.log(e)
     if (e.detail.userInfo) {
-      //用户按了允许授权按钮
+      // 用户按了允许授权按钮
       app.globalData.userInfo = e.detail.userInfo
       this.setData({
         userInfo: e.detail.userInfo,
         hasUserInfo: true
       })
     } else {
-      //用户按了拒绝按钮
+      // 用户按了拒绝按钮
       wx.showModal({
         title: '警告',
-        content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
+        content: '2您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
         showCancel: false,
         confirmText: '返回授权',
         success: function (res) {
